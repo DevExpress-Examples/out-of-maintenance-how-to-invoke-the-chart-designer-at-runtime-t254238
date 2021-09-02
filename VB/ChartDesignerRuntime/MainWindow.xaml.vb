e31c4fdd0f3bfd6b47608af03d561f46d01dcced
DevExpressExampleBot <fakeEmail>
2021-09-02 16:48:59 +0300

    Source auto update [skip ci]

--- VB/ChartDesignerRuntime/MainWindow.xaml.vb
+++ VB/ChartDesignerRuntime/MainWindow.xaml.vb
@@ -3,18 +3,18 @@ Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Ribbon
 
 Namespace ChartDesignerRuntime
-    Partial Public Class MainWindow
-        Inherits DXRibbonWindow
+	Partial Public Class MainWindow
+		Inherits DXRibbonWindow
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        #Region "#ShowDesigner"
-        Private Sub btnChartDesigner_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-            Dim designer As New ChartDesigner(chartControl)
-            designer.Show(Me)
-        End Sub
-        #End Region ' #ShowDesigner
-    End Class
+		#Region "#ShowDesigner"
+		Private Sub btnChartDesigner_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+			Dim designer As New ChartDesigner(chartControl)
+			designer.Show(Me)
+		End Sub
+		#End Region ' #ShowDesigner
+	End Class
 End Namespace
